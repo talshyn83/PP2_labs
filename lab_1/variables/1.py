257710e2f602da8cebc7b2bcb5b40f88c023a620
Binur <b_akhmet@kbtu.kz>
2025-01-24 23:23:58 +0500

    Add files via upload

---
+++ lab_1/variables/1.py
@@ -0,0 +1,6 @@
+x = 5          #int
+y = "Bikosh"   #str
+z = 4.09       #float
+print(type(x))
+print(type(y))
+print(type(z))
---
+++ lab_1/variables/2.py
@@ -0,0 +1,13 @@
+# variable name
+myvar = "Bikosh"
+my_var = "Bikosh"
+_my_var = "Bikosh"
+myVar = "Bikosh"
+MYVAR = "Bikosh"
+myvar2 = "Bikosh"
+
+
+# variable name error
+'''2myvar = "Bikosh"
+my-var = "Bikosh"
+my var = "Bikosh" '''
